b81173bee57cfded2aea421f88a1f785a841403f
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-05-08 16:23:01 -0300

    first commit!

---
+++ NewsiOS/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsiOS
+//
+//  Created by Marcel Leite de Farias on 08/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
